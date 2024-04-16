a421d78fd086fcf7a3a8436e086e8c35a15de76d
Bogdan Vlad <vladbgd@gmail.com>
2024-04-16 09:58:26 +0300

    Avoid infinite loop.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
 import { ViteDevServer } from "vite";
 import chokidar from "chokidar";
 import path from "path";
+
+let watcher: chokidar.FSWatcher|undefined;
+let viteServer: ViteDevServer|undefined;
+
+function watchAndRestart(server: ViteDevServer) {
+    viteServer = server;
+    if (watcher) {
+        return
+    }
+    const directoryToWatch = path.join(".", "node_modules", "@genezio-sdk");
+    watcher = chokidar.watch(directoryToWatch, {
+      persistent: true,
+    });
+    
+    watcher.on("all", (event: string, filePath: string) => {
+      const filePathComponents = filePath.split(path.sep);
+      if (
+        (event == "change" ||
+        event == "add") &&
+        filePathComponents[filePathComponents.length - 1] == "package.json"
+      ) {
+        viteServer?.restart(true);
+      }
+    });
+}
+
 export default function genezioLocalSDKReload() {
   return {
     name: "vite-plugin-genezio",
     configureServer: (server: ViteDevServer): void => {
-      const directoryToWatch = path.join(".", "node_modules", "@genezio-sdk");
-      const watcher = chokidar.watch(directoryToWatch, {
-        persistent: true,
-      });
-
-      // Watch for adding the package.json file to restart the server only once,
-      // then only watch for changes to the package.json file.
-      const addWatcher = watcher.on("add", (filePath: string) => {
-        const filePathComponents = filePath.split(path.sep);
-        if (
-          filePathComponents[filePathComponents.length - 1] == "package.json"
-        ) {
-          addWatcher.close();
-          server.restart(true);
-        }
-      });
-
-      watcher.on("change", (filePath: string) => {
-        const filePathComponents = filePath.split(path.sep);
-        if (
-          filePathComponents[filePathComponents.length - 1] == "package.json"
-        ) {
-          server.restart(true);
-        }
-      });
+      watchAndRestart(server);
 
       // Exclude @genezio-sdk from optimization because it is usually
       // replaced by our watcher at runtime.
